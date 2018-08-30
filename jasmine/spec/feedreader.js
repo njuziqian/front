3b2bf01243851cfad0a54722f24add7968323640
daiwei <dw@nju.edu.cn>
2018-08-30 16:46:08 +0800

    i have finished the project

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,7 +8,7 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function() {
+ $(function() {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
@@ -21,10 +21,12 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+
+
+
+         it('are defined', function() {
             expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
+            expect(allFeeds.length).not.toBe(0);});
 
 
         /* TODO: Write a test that loops through each feed
@@ -33,40 +35,136 @@ $(function() {
          */
 
 
+
+         describe('遍历 allFeeds 对象里面的所有的源',function(){
+            let urlIsEmpty=false;
+            let nameIsEmpty=false;
+            function emptyTest(str){
+             if(str == null || str == undefined || str == ''||str.length==0){
+                return true;
+            }
+            const regu =new RegExp( '^[ ]+$');
+            if(regu.test(str))
+            {
+               return true;
+           }
+       }
+       allFeeds.forEach(function(feed){
+        let feedUrl = feed.url,
+        feedName = feed.name;
+        if(emptyTest(feedUrl)){
+            urlIsEmpty=true;
+        }
+
+        if(emptyTest(feedName)){
+            nameIsEmpty=true;
+        }
+    });
+       it('保证有链接字段而且链接不是空的',function(){
+        expect(urlIsEmpty).toBe(false);
+    });
+
+
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
+         it('保证有名字字段而且不是空的',function(){
+           expect(nameIsEmpty).toBe(false);
+       });
+     });
+
+     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
+    describe('The menu',function(){
+  /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+         it('ensures the menu element is hidden by default',function(){
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('visibility when the menu icon is clicked and  hide when clicked again',function(){
+            let menuicon= $('a.menu-icon-link');
+            if($('body').hasClass('menu-hidden')){
+                menuicon.click();
+                expect($('body').hasClass('menu-hidden')).toBe(false);
+            }
+            if(!($('body').hasClass('menu-hidden'))){
+                menuicon.click();
+                expect($('body').hasClass('menu-hidden')).toBe(true);
+            }
+        });
+
+      });
+
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+    describe('Initial Entries',function(){
+ /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+         beforeEach(function(done){
+            loadFeed(0,function(){
+                done();
+            });
+        });
+         it('the loadFeed function is called and completes its work',function(done){
+            // alert($('.feed').find('.entry').length);
+            expect( $('.feed').find('.entry').length>0).toBe(true);
+            done()
+        });
+     });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection',function(){
+ /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+         let before,now;
+         beforeEach(function(done){
+            jasmine.DEFAULT_TIMEOUT_INTERVA=500000;
+            loadFeed(1,function(){
+
+                before=$('.entry h2').html();
+                console.log('1 finished');
+                loadFeed(2,function(){
+                    now=$('.entry h2').html();
+                    console.log('2 finished');
+                    done();
+                });
+            });
+
+        });
+         it('when a new feed is loaded by the loadFeed function that the content actually changes',function(done){
+            console.log(now.length,before.length);
+            expect( before!=now).toBe(true);
+            done();
+        });
+
+     });
+
+    /*
+    测试未定义变量和数组越界
+
+    */
+    describe('未定义变量和数组越界',function(){
+
+    })
 }());
--- js/app.js
+++ js/app.js
@@ -8,26 +8,26 @@
 
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
-    {
-        name: 'Udacity Blog',
-        url: 'http://blog.udacity.com/feed'
-    }, {
-        name: 'CSS Tricks',
-        url: 'http://feeds.feedburner.com/CssTricks'
-    }, {
-        name: 'HTML5 Rocks',
-        url: 'http://feeds.feedburner.com/html5rocks'
-    }, {
-        name: 'Linear Digressions',
-        url: 'http://feeds.feedburner.com/udacity-linear-digressions'
-    }
+{
+    name: 'Udacity Blog',
+    url: 'http://blog.udacity.com/feed'
+}, {
+    name: 'CSS Tricks',
+    url: 'http://feeds.feedburner.com/CssTricks'
+}, {
+    name: 'HTML5 Rocks',
+    url: 'http://feeds.feedburner.com/html5rocks'
+}, {
+    name: 'Linear Digressions',
+    url: 'http://feeds.feedburner.com/udacity-linear-digressions'
+}
 ];
 
 /* This function starts up our application. The Google Feed
  * Reader API is loaded asynchonously and will then call this
  * function when the API is loaded.
  */
-function init() {
+ function init() {
     // Load the first feed we've defined (index of 0).
     loadFeed(0);
 }
@@ -41,21 +41,45 @@ function init() {
  * which will be called after everything has run successfully.
  */
  function loadFeed(id, cb) {
-     var feedUrl = allFeeds[id].url,
-         feedName = allFeeds[id].name;
+    //变量id不可越界或者未定义
+    if(id==undefined||id>=allFeeds.length||id<0 ||id==null||(typeof id)!='number' )
+    {
+       throw new Error('变量id不可越界、未定义、空、或者非数字');
+   }   
+   var feedUrl = allFeeds[id].url,
+   feedName = allFeeds[id].name;
 
-     $.ajax({
+   $.ajax({
        type: "POST",
        url: 'https://rsstojson.udacity.com/parseFeed',
        data: JSON.stringify({url: feedUrl}),
        contentType:"application/json",
        success: function (result, status){
 
-                 var container = $('.feed'),
-                     title = $('.header-title'),
-                     entries = result.feed.entries,
-                     entriesLen = entries.length,
-                     entryTemplate = Handlebars.compile($('.tpl-entry').html());
+         var container = $('.feed'),
+         title = $('.header-title'),
+         entries = result.feed.entries;
+
+         /*
+          *检查返回结果是否包含预期的feed数据  
+         */
+         let entriesLen;
+         if(entries==undefined){
+            entriesLen=0;
+        }
+        else
+        {
+            entriesLen = entries.length;
+        }
+        if(entriesLen==0)
+        {
+            alert('there’s nothing to show!');
+
+        }
+        else{
+
+
+         let entryTemplate = Handlebars.compile($('.tpl-entry').html());
 
                  title.html(feedName);   // Set the header text
                  container.empty();      // Empty out all previous entries
@@ -65,39 +89,41 @@ function init() {
                   * entryTemplate (created above using Handlebars) and append
                   * the resulting HTML to the list of entries on the page.
                   */
-                 entries.forEach(function(entry) {
+                  entries.forEach(function(entry) {
                      container.append(entryTemplate(entry));
                  });
-
-                 if (cb) {
-                     cb();
-                 }
-               },
-       error: function (result, status, err){
+              } 
+              if (cb) {
+                 cb();
+             }
+         },
+         error: function (result, status, err){
                  //run only the callback without attempting to parse result due to error
+                console.log(status);
+                console.log(err);
                  if (cb) {
                      cb();
                  }
-               },
-       dataType: "json"
-     });
- }
+             },
+             dataType: "json"
+         });
+}
 
 /* Google API: Loads the Feed Reader API and defines what function
  * to call when the Feed Reader API is done loading.
  */
-google.setOnLoadCallback(init);
+ google.setOnLoadCallback(init);
 
 /* All of this functionality is heavily reliant upon the DOM, so we
  * place our code in the $() function to ensure it doesn't execute
  * until the DOM is ready.
  */
-$(function() {
+ $(function() {
     var container = $('.feed'),
-        feedList = $('.feed-list'),
-        feedItemTemplate = Handlebars.compile($('.tpl-feed-list-item').html()),
-        feedId = 0,
-        menuIcon = $('.menu-icon-link');
+    feedList = $('.feed-list'),
+    feedItemTemplate = Handlebars.compile($('.tpl-feed-list-item').html()),
+    feedId = 0,
+    menuIcon = $('.menu-icon-link');
 
     /* Loop through all of our feeds, assigning an id property to
      * each of the feeds based upon its index within the array.
@@ -105,7 +131,7 @@ $(function() {
      * above using Handlebars) and append it to the list of all
      * available feeds within the menu.
      */
-    allFeeds.forEach(function(feed) {
+     allFeeds.forEach(function(feed) {
         feed.id = feedId;
         feedList.append(feedItemTemplate(feed));
 
@@ -116,7 +142,7 @@ $(function() {
      * the menu, load the feed, and prevent the default action
      * (following the link) from occurring.
      */
-    feedList.on('click', 'a', function() {
+     feedList.on('click', 'a', function() {
         var item = $(this);
 
         $('body').addClass('menu-hidden');
@@ -127,7 +153,7 @@ $(function() {
     /* When the menu icon is clicked on, we need to toggle a class
      * on the body to perform the hiding/showing of our menu.
      */
-    menuIcon.on('click', function() {
+     menuIcon.on('click', function() {
         $('body').toggleClass('menu-hidden');
     });
-}());
+ }());
